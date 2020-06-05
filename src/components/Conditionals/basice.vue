f1b3a8a02381eb03444e026d13ade29a8fe237ac
gopikrishnap <gopikrishnaponnoju@gmail.com>
2020-06-05 13:31:33 +0530

    CssStyle class commit

--- src/components/Conditionals/basice.vue
+++ src/components/Conditionals/basice.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app" class="container" style="margin-top:120px">
-    <h4 v-once>{{title}}</h4>
+    <h4 v-once>{{ title }}</h4>
     <hr />
     <h1>
       {{ Sayhello() }} -
@@ -10,24 +10,54 @@
     <h1 v-html="FinishedLikHTMl"></h1>
     <hr />
 
-    <button class="btn btn-primary" v-on:click="Increase">Increase counter one</button>
-    <button class="btn btn-primary" v-on:click="Increasetwo(2,$event)">Increase counter two</button>
-    <p>{{counter}}</p>
+    <button class="btn btn-primary" v-on:click="Increase">
+      Increase counter one
+    </button>
+    <button class="btn btn-primary" v-on:click="Increasetwo(2, $event)">
+      Increase counter two
+    </button>
+    <p>{{ counter }}</p>
     <hr />
-    <p v-on:mousemove="Updatecoordinates">Coordinates {{x}} / {{y}}</p>
-    <hr />
+    <p v-on:mousemove="Updatecoordinates">
+      Coordinates {{ x }} / {{ y }}
 
-    <input type="text" v-on:keyup.enter.space="Alert" />
-    <!-- // v-on:keyup -->
+      <span v-on:mousemove.stop> DEAD SPORT</span>
+    </p>
+    <hr />
+    <p>Key up event</p>
+    <p>
+      <input type="text" v-on:keyup.enter.space="Alert" />
+      <!-- // v-on:keyup -->
+    </p>
+
     <hr />
     <h1>Two Way Data Bind</h1>
     <input type="text " v-model="twowaybind" />
 
-    <h1>{{twowaybind}}</h1>
+    <h1>{{ twowaybind }}</h1>
     <hr />
-    <button  class="btn btn-primary" v-on:click="increasemethod">Grater and smaller</button>
-    <p>{{counter}}</p>
-    <p>{{result}}</p>
+    <button class="btn btn-primary" v-on:click="increasemethod">
+      Grater and smaller
+    </button>
+    <p>{{ counter }}</p>
+    <p>{{ result }}</p>
+    <hr />
+    <h1>CSS class bind</h1>
+    <input type="text" placeholder="Css" v-model="color" />
+    <input type="text" placeholder="Style" v-model="width" />
+    <div>
+      <!-- <div class="demo" @click="attachRed=!attachRed" v-bind:class="{red:attachRed,blue:!attachRed}"></div> -->
+      <div
+        class="demo"
+        @click="attachRed = !attachRed"
+        v-bind:class="divClass"
+      ></div>
+      <div class="demo" v-bind:class="{ red: attachRed }"></div>
+      <div class="demo" :class="color"></div>
+      <div class="demo" :style="{ backgroundColor: color }"></div>
+      <div class="demo" :style="DivStyle"></div>
+      <div class="demo" :style="[DivStyle,{height: width +'px'}]"></div>
+    </div>
   </div>
 </template>
 
@@ -42,8 +72,11 @@ export default {
       x: 0,
       y: 0,
       twowaybind: "",
-      result: ""
-    }
+      result: "",
+      attachRed: false,
+      color: "green",
+      width: 100
+    };
   },
   methods: {
     Sayhello: function() {
@@ -68,6 +101,40 @@ export default {
       this.counter++;
       this.result = this.counter > 5 ? "Grater 5" : "Smaller 5";
     }
+  },
+  computed: {
+    divClass() {
+      return {
+        red: this.attachRed,
+        blue: !this.attachRed
+      };
+    },
+    DivStyle() {
+      return {
+        backgroundColor: this.color,
+        width: this.width + "px"
+      };
+    }
   }
 };
 </script>
+<style scoped>
+.demo {
+  width: 100%;
+  height: 100px;
+  background-color: gray;
+  display: inline-block;
+  margin: 10px;
+}
+.red {
+  background-color: red;
+}
+
+.blue {
+  background-color: blue;
+}
+
+.green {
+  background-color: green;
+}
+</style>
