98ddf265ba81758b27de33bcb3a88f679dc7ee18
gopikrishnap <gopikrishnaponnoju@gmail.com>
2020-06-05 14:29:03 +0530

    axios changes commit

--- src/components/Conditionals/advanceDirectives.vue
+++ src/components/Conditionals/advanceDirectives.vue
@@ -19,6 +19,25 @@
 </template>
 
 <script>
+
+import Vue from 'vue'
+Vue.directive('highlight', {
+  bind(el, binding, vnode) {
+      // el.style.backgroundColor = 'green';
+      // el.style.backgroundColor = binding.value;
+      var delay = 0;
+      if (binding.modifiers['delayed']) {
+          delay = 3000;
+      }
+      setTimeout(() => {
+          if (binding.arg == 'background') {
+              el.style.backgroundColor = binding.value;
+          } else {
+              el.style.color = binding.value;
+          }
+      }, delay);
+  }
+});
     export default {
         directives: {
             'local-highlight': {
--- src/main.js
+++ src/main.js
@@ -13,23 +13,7 @@ Vue.use(VueRouter);
 Vue.use(vueResource);
 Vue.use(vuelidate);
 
-Vue.directive('highlight', {
-  bind(el, binding, vnode) {
-      // el.style.backgroundColor = 'green';
-      // el.style.backgroundColor = binding.value;
-      var delay = 0;
-      if (binding.modifiers['delayed']) {
-          delay = 3000;
-      }
-      setTimeout(() => {
-          if (binding.arg == 'background') {
-              el.style.backgroundColor = binding.value;
-          } else {
-              el.style.color = binding.value;
-          }
-      }, delay);
-  }
-});
+
 
 const router = new VueRouter({
   routes,
